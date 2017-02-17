ad7e530e139ba2403f36c5f37e7451674d015858
Dmitry Labutin <dmitry@labutin.net>
2017-02-17 11:18:33 +0300

    Changed TTL logic

--- kvstorage/storage.go
+++ kvstorage/storage.go
@@ -10,15 +10,22 @@ import (
 
 const TTLTimeout = 60 * time.Second
 
+type ttlValue struct {
+	sync.RWMutex
+	keys []string
+}
+
 type Storage struct {
 	cmap           concurrent_map.CMapInterface
 	ttl            concurrent_map.CMapInterface
+	ttlMutex       sync.Mutex
 	done           chan interface{}
 	wg             *sync.WaitGroup
 	lastClearedTTL int64
 	ttlTimeout     time.Duration
 }
 
+// NewKVStorage creates new key value storage
 func NewKVStorage(chunks uint32, startTTLRemoval bool) *Storage {
 	kvstorage := &Storage{}
 	kvstorage.cmap = concurrent_map.NewCMap(chunks)
@@ -33,19 +40,28 @@ func NewKVStorage(chunks uint32, startTTLRemoval bool) *Storage {
 	return kvstorage
 }
 
+// ensureTTLKey creates empty record with given key in ttl map
+func (t *Storage) ensureTTLKey(key string) *ttlValue {
+	t.ttlMutex.Lock()
+	defer t.ttlMutex.Unlock()
+	if res, ok := t.ttl.Get(key); !ok {
+		newValue := &ttlValue{}
+		t.ttl.Put(key, newValue)
+		return newValue
+	} else {
+		return res.(*ttlValue)
+	}
+}
+
 // Set stores value for given key and TTL
 func (t *Storage) Set(key string, value interface{}, TTL time.Duration) {
 	t.cmap.Put(key, value)
 	if TTL > 0 {
-		value, ok := t.ttl.Get(key)
-		var keys []string
-		if !ok {
-			keys = []string{}
-		} else {
-			keys = value.([]string)
-		}
-		keys = append(keys, key)
-		t.ttl.Put(strconv.FormatInt(time.Now().Add(TTL).Unix(), 10), keys)
+		ttlKey := strconv.FormatInt(time.Now().Add(TTL).Unix(), 10)
+		ttlRecord := t.ensureTTLKey(ttlKey)
+		ttlRecord.Lock()
+		ttlRecord.keys = append(ttlRecord.keys, key)
+		ttlRecord.Unlock()
 	} else {
 		if TTL < 0 {
 			t.cmap.Remove(key)
@@ -78,6 +94,16 @@ func (t *Storage) Get(key string) (interface{}, bool) {
 	return tValue, true
 }
 
+func (t *Storage) GetTTL(key string) (interface{}, bool) {
+	value, ok := t.ttl.Get(key)
+	if !ok {
+		return nil, false
+	}
+	tValue := value
+
+	return tValue, true
+}
+
 // GetListElement returns i-th element from List value
 func (t *Storage) GetListElement(key string, i int) (interface{}, error) {
 	value, ok := t.Get(key)
@@ -94,6 +120,7 @@ func (t *Storage) GetListElement(key string, i int) (interface{}, error) {
 	}
 }
 
+// GetDictElement returns element with key 'dictKey' from dictionary with given key
 func (t *Storage) GetDictElement(key, dictKey string) (interface{}, error) {
 	value, ok := t.Get(key)
 	if !ok {
@@ -115,13 +142,18 @@ func (t *Storage) Keys() []string {
 	return t.cmap.Keys()
 }
 
+// TTLKeys returns all TTL keys
+func (t *Storage) TTLKeys() []string {
+	return t.ttl.Keys()
+}
+
 // clearTTLExpiredRecords removes old records from map
 func (t *Storage) clearTTLExpiredRecords() {
 	lastTime := time.Now().Unix()
 	for i := t.lastClearedTTL; i < lastTime; i++ {
 		strI := strconv.FormatInt(i, 10)
 		if value, ok := t.ttl.Get(strI); ok {
-			keysToRemove := value.([]string)
+			keysToRemove := value.(*ttlValue).keys
 			for _, key := range keysToRemove {
 				t.Remove(key)
 			}
